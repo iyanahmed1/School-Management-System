4ebcdb4b35b88167f86d6b2cc6a2d85a754be305
iyanahmed1 <malumasaid123@gmail.com>
2024-10-10 23:33:12 +0300

    commit

--- TeacherPage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from TeacherPage import views 
-
-app_name='TeacherPage'
-
-urlpatterns=[
-    path('attendanceview/',views.attendanceview,name='attendanceview'),
-    path('gradeview/',views.gradeview,name='gradeview'),
-]
